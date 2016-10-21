5b996718403bac7f5d1c7209148984d662c3cc62
Aaron Milam <aaron@lesson.ly>
2016-10-20 20:51:26 -0400

    Through hole 5

--- lib/golf.rb
+++ lib/golf.rb
@@ -14,14 +14,21 @@ class Golf
 
   def self.hole4(a)
     a.map { |t| t.split /\(|\)/ }.map do |t, n|
-      case t
-      when "man" then "hat(man(#{n}))"
-      when "dog" then "dog(#{n}(bone))"
-      when "cat" then "dead(#{n})"
+      case t[0]
+      when "m" then "hat(man(#{n}))"
+      when "d" then "dog(#{n}(bone))"
+      when "c" then "dead(#{n})"
       end
     end
   end
 
-  def self.hole5
+  def self.hole5(a)
+    n = []
+    (0..(a.size-1)).each { |l|
+      (0..(3-l)).each { |i|
+        n << a[i..(i+l)]
+      }
+    }
+    n
   end
 end
