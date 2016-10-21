137191fadafd1a2da7cb4b248aee3b9c40af3444
Aaron Milam <aaron@lesson.ly>
2016-10-20 20:21:13 -0400

    Holes 1-4

--- lib/golf.rb
+++ lib/golf.rb
@@ -1,2 +1,27 @@
 class Golf
+
+  def self.hole1(a)
+    a.reduce 1, :*
+  end
+
+  def self.hole2
+    ("a".."z").to_a
+  end
+
+  def self.hole3(i)
+    hole1(1..i)
+  end
+
+  def self.hole4(a)
+    a.map { |t| t.split /\(|\)/ }.map do |t, n|
+      case t
+      when "man" then "hat(man(#{n}))"
+      when "dog" then "dog(#{n}(bone))"
+      when "cat" then "dead(#{n})"
+      end
+    end
+  end
+
+  def self.hole5
+  end
 end
